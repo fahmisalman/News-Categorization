f80ec7aa89c7c3b4a7a9a483e780af94cc4ae79f
fahminurfikri <fahmisalman@gmail.com>
2017-05-07 22:23:10 +0700

    Preprocessing

--- Test.py
+++ Test.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.stem.porter import *
 
@@ -5,8 +6,8 @@ import glob
 import re
 
 def loadData(location):
-    n = 0
     bag = []
+    n = 0
     for name in glob.glob(location):
         file = open(name)
         s = file.read()
@@ -35,16 +36,27 @@ def stopwordRemoval(token):
             temp.append(token[i])
     return temp
 
-if __name__ == '__main__':
-    sport, nSport = loadData('bbc-2/sport/*')
-    business, nBusiness = loadData('bbc-2/business/*')
-    politics, nPolitics = loadData('bbc-2/politics/*')
-    entertainment, nEntertainment = loadData('bbc-2/entertainment/*')
-    tech, nTech = loadData('bbc-2/tech/*')
-    bag = sport + business + politics + entertainment + tech
-    print len(bag)
-    wordList = list(set(bag))
+def likelihoodProbabilities(list, bag):
     likelihood = []
-    for i in range(len(wordList)):
-        likelihood.append(bag.count(wordList[i]))
-    print sum(likelihood)
+    for i in range(len(list)):
+        likelihood.append(bag.count(list[i]))
+    for i in range(len(likelihood)):
+        likelihood[i] += 1
+    total = sum(likelihood)
+    for i in range(len(likelihood)):
+        likelihood[i] /= total
+    return likelihood
+
+if __name__ == '__main__':
+    sport, nSport = loadData('bbc-2/sport/001.txt')
+    business, nBusiness = loadData('bbc-2/business/001.txt')
+    politics, nPolitics = loadData('bbc-2/politics/001.txt')
+    entertainment, nEntertainment = loadData('bbc-2/entertainment/001.txt')
+    tech, nTech = loadData('bbc-2/tech/001.txt')
+    bag = sport + business + politics + entertainment + tech
+    wordList = list(set(bag))
+    likelihoodSport = likelihoodProbabilities(wordList, sport)
+    likelihoodBusiness = likelihoodProbabilities(wordList, business)
+    likelihoodPolitics = likelihoodProbabilities(wordList, politics)
+    likelihoodEntertainment = likelihoodProbabilities(wordList, entertainment)
+    likelihoodTech = likelihoodProbabilities(wordList, tech)
