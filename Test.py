4ee48790fd4056e38f4ea36d40051a93ccbce859
fahminurfikri <fahmisalman@gmail.com>
2017-05-07 10:25:42 +0700

    Preprocessing

--- Test.py
+++ Test.py
@@ -1,16 +1,33 @@
 from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.porter import *
 
 import glob
 import re
 
-
-bag = []
+def loadData(location, label):
+    n = 0
+    bag = []
+    for name in glob.glob(location):
+        file = open(name)
+        s = file.read()
+        s = s.lower()
+        s = re.sub(r'[^a-z]', ' ', s)
+        bag += s.split()
+        n += 1
+    bag = stopwordRemoval(bag)
+    bag = lemmatization(bag)
+    temp = []
+    for i in range(len(bag)):
+        temp.append([bag[i], label])
+    return temp, n
 
 def lemmatization(token):
     lemma = WordNetLemmatizer()
+    stemmer = PorterStemmer()
     temp = []
     for i in range(len(token)):
-        temp.append(lemma.lemmatize(token[i]))
+        word = stemmer.stem(token[i])
+        temp.append(lemma.lemmatize(word))
     return temp
 
 def stopwordRemoval(token):
@@ -22,16 +39,25 @@ def stopwordRemoval(token):
     return temp
 
 if __name__ == '__main__':
-    for name in glob.glob('bbc-2/sport/*'):
-        file = open(name)
-        s = file.read()
-        s = s.lower()
-        s = re.sub(r'[^a-z]', ' ', s)
-        bag += s.split()
-    bag = stopwordRemoval(bag)
-    bag = lemmatization(bag)
-    # print bag
-    a = []
-    for i in range(len(bag)):
-        a.append(bag[i])
-    print a
+    # for name in glob.glob('bbc-2/sport/*'):
+    #     file = open(name)
+    #     s = file.read()
+    #     s = s.lower()
+    #     s = re.sub(r'[^a-z]', ' ', s)
+    #     bag += s.split()
+    sportLoc = 'bbc-2/sport/*'
+    businessLoc = 'bbc-2/business/*'
+    politicsLoc = 'bbc-2/politics/*'
+    entertainmentLoc = 'bbc-2/entertainment/*'
+    techLoc = 'bbc-2/tech/*'
+    sport, nSport = loadData(sportLoc, "sport")
+    business, nBusiness = loadData(businessLoc, "business")
+    politics, nPolitics = loadData(politicsLoc, "politics")
+    entertainment, nEntertainment = loadData(entertainmentLoc, "entertainment")
+    tech, nTech = loadData(techLoc, "tech")
+    print nSport, nBusiness, nPolitics, nEntertainment, nTech
+    print len(sport), len(business), len(politics), len(entertainment), len(tech)
+    bag = sport + business + politics + entertainment + tech
+    print len(bag)
+    # bag = stopwordRemoval(bag)
+    # bag, word = lemmatization(bag)
